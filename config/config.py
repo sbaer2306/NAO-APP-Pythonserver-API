9f09b4d3e581a43f66f6b00c02d90f863c175626
sbaer2306 <sebastian.baer98@gmail.com>
2023-06-30 15:05:18 +0200

    Hihihi das wird bestimmt funktionieren

--- config/config.py
+++ config/config.py
@@ -2,24 +2,15 @@ from flask import jsonify
 from robot_controller.config import NAO_PORT,NAO_IP_ADDRESS
 from naoqi import ALProxy
 
-
 def connect(request):
     try:
         NAO_IP_ADDRESS = request.json.get('ip_address')
         NAO_PORT = request.json.get('port')
-
-        if NAO_IP_ADDRESS is None or NAO_PORT is None:
-            raise ValueError("Both 'ip_address' and 'port' parameters are required")
-
         tts_proxy = ALProxy("ALTextToSpeech", NAO_IP_ADDRESS, NAO_PORT)
-        tts_proxy.say("NAO-APP connected")
-
+        tts_proxy.say("Voice changed to" + voice)
         return jsonify({'message': 'Successfully connected', 'return_code': 200}), 200
-    except ValueError as ve:
-        return jsonify({'message': 'Failed to connect', 'cause': str(ve), 'return_code': 400}), 400
     except Exception as e:
-        return jsonify({'message': 'Failed to connect', 'cause': str(e), 'return_code': 500}), 500
-
+        return jsonify({'message': 'Failed to connect','cause': str(e), 'return_code': 500}), 500
 
 def get_temperature():
     try:
